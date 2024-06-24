61a683d985bd1ce37a4cc0a7c3dd5cf0de1aebaf
abhinav2210103 <abhinav2210103@akgec.ac.in>
2024-06-24 15:32:42 +0530

    Added Validations using Formik and Yup

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,49 +1,64 @@
 'use client';
 import React, { useState } from 'react';
 import axios from 'axios';
-import "../app/globals.css"
+import "../app/globals.css";
 import RootLayout from '@/app/layout';
 import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import { useFormik } from 'formik';
+import validationSchema from '@/app/constants/validationSchema';
 
-export default function login(): React.ReactElement {
+export default function Login(): React.ReactElement {
   return (
     <GoogleReCaptchaProvider reCaptchaKey={"6LduePwpAAAAAPSYw03svKMQfz93zdOgQJl5r6ZN"} scriptProps={{ async: true }}>
-      <LoginComponent/>
+      <LoginComponent />
     </GoogleReCaptchaProvider>
   );
 }
 
 const LoginComponent = () => {
-  const [studentNumber, setStudentNumber] = useState('');
-  const [password, setPassword] = useState('');
   const { executeRecaptcha } = useGoogleReCaptcha();
   const [disabled, setDisabled] = useState(false);
 
-  const handleLogin = async () => {
-    try {
-      if (!executeRecaptcha) {
-        console.error('ReCAPTCHA not available');
-        return;
-      }
-      const token = await executeRecaptcha('Login');
-      const response = await axios.post(
-        'https://cine-student.onrender.com/student/login',
-        { studentNumber, password, token},
-        {
-          withCredentials: true,
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    resetForm
+  } = useFormik({
+    initialValues: {
+      studentNumber: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      setDisabled(true);
+      try {
+        if (!executeRecaptcha) {
+          console.error('ReCAPTCHA not available');
+          return;
         }
-      );
+        const token = await executeRecaptcha('Login');
+        const response = await axios.post(
+          'https://cine-student.onrender.com/student/login',
+          { ...values, token },
+          {
+            withCredentials: true,
+          }
+        );
 
-      console.log('Login successful:', response);
-      setStudentNumber('');
-      setPassword('');
-    } catch (error) {
-      console.error('Login failed:', error);
-    } finally {
-      setDisabled(false);
-    }
-  };
+        console.log('Login successful:', response);
+        resetForm();
+      } catch (error) {
+        console.error('Login failed:', error);
+      } finally {
+        setDisabled(false);
+      }
+    },
+  });
 
   return (
     <div className='relative min-h-[100vh] w-full'
@@ -54,8 +69,8 @@ const LoginComponent = () => {
       }}>
       <div className='relative bg-[#ffffff] bg-opacity-50 h-[10vh] w-full flex items-center justify-center gap-5'>
         <img src='./csilogo.svg'/>
-      <div className='font-medium text-4xl'>CSI Exam Portal</div>  
-      </div>   
+        <div className='font-medium text-4xl'>CSI Exam Portal</div>
+      </div>
 
       <div className='flex items-center justify-center min-h-[85vh]'>
         <div className='relative flex flex-col justify-between items-center gap-5 p-16 px-25' style={{
@@ -68,41 +83,51 @@ const LoginComponent = () => {
           <div>
             <p className='font-semibold text-3xl'>Cine-2024</p>
           </div>
-          <div className='flex flex-col'>
-            <label htmlFor="studentNumber">Student Number:</label>
-            <div className='border-4'>
-            <input
-              id="studentNumber"
-              type="number"
-              value={studentNumber}
-              onChange={(e) => setStudentNumber(e.target.value)}
-            />
+          <form onSubmit={handleSubmit} className="w-full flex flex-col gap-5">
+            <div className='flex flex-col'>
+              <label htmlFor="studentNumber">Student Number:</label>
+              <div className='border-4'>
+                <input
+                  id="studentNumber"
+                  type="text"
+                  name="studentNumber"
+                  placeholder="Enter Your Student Number"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.studentNumber}
+                />
+              </div>
+              {touched.studentNumber && errors.studentNumber ? (
+                <div className="text-red-500">{errors.studentNumber}</div>
+              ) : null}
             </div>
-          </div>
-          <div className='flex flex-col'>
-            <label htmlFor="password">Password:</label>
-            <input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className='border-4'
-            />
-          </div>
-          <div>
-            <button
-              onClick={() => {
-                if (!disabled) {
-                  setDisabled(true);
-                  handleLogin();
-                }
-              }}
-              disabled={disabled}
-              className='border-2 m-5 px-5 py-1'
-            >
-              Login
-            </button>
-          </div>
+            <div className='flex flex-col'>
+              <label htmlFor="password">Password:</label>
+              <div className='border-4'>
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter Password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+              </div>
+              {touched.password && errors.password ? (
+                <div className="text-red-500">{errors.password}</div>
+              ) : null}
+            </div>
+            <div>
+              <button
+                type="submit"
+                disabled={disabled}
+                className='border-2 m-5 px-5 py-1'
+              >
+                Login
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
